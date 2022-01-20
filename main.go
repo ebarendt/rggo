99718a5015238c08baf8dd8740e368219ccebc7b
Eric Barendt <eric@barendt.org>
2022-01-20 10:01:18 -0800

    Sleep to give the browser time to open the file

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 )
 
 const (
@@ -70,6 +71,8 @@ func run(filename string, out io.Writer, skipPreview bool) error {
 		return nil
 	}
 
+	defer os.Remove(outName)
+
 	return preview(outName)
 }
 
@@ -111,5 +114,7 @@ func preview(fname string) error {
 		return err
 	}
 
-	return exec.Command(cPath, cParams...).Run()
+	err = exec.Command(cPath, cParams...).Run()
+	time.Sleep(2 * time.Second)
+	return err
 }
