36fd80ca900904c6a798e019e32867d8d920349e
Eric Barendt <eric@barendt.org>
2022-02-01 12:55:48 -0800

    goroutines process files in parallel

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -40,24 +41,49 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 
 	consolidate := make([]float64, 0)
 
+	resCh := make(chan []float64)
+	errCh := make(chan error)
+	doneCh := make(chan struct{})
+	wg := sync.WaitGroup{}
+
 	for _, fname := range filenames {
-		f, err := os.Open(fname)
-		if err != nil {
-			return fmt.Errorf("Cannot open file: %w", err)
-		}
+		wg.Add(1)
+		go func(fname string) {
+			defer wg.Done()
 
-		data, err := csv2float(f, column)
-		if err != nil {
-			return err
-		}
+			f, err := os.Open(fname)
+			if err != nil {
+				errCh <- fmt.Errorf("Cannot open file: %w", err)
+				return
+			}
 
-		if err := f.Close(); err != nil {
-			return err
-		}
+			data, err := csv2float(f, column)
+			if err != nil {
+				errCh <- err
+			}
 
-		consolidate = append(consolidate, data...)
+			if err := f.Close(); err != nil {
+				errCh <- err
+			}
+
+			resCh <- data
+		}(fname)
 	}
 
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case data := <-resCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
+			return err
+		}
+	}
 }
